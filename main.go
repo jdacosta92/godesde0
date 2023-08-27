f649b95329611a70d31faeb770c4e43f861a4836
jdacosta92 <juliandaniel.acosta@gmail.com>
2023-08-27 15:29:06 -0300

    siguiente commit

--- main.go
+++ main.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/jdacosta92/godesde0/variables"
+	"runtime"
 )
 
 func main() {
-	estado, resultado := variables.ConviertoaTexto(1588)
+	/*estado, resultado := variables.ConviertoaTexto(1588)
 	fmt.Println(estado)
-	fmt.Println(resultado)
+	fmt.Println(resultado)*/
+
+	if os := runtime.GOOS; os == "linux" || os == "OS X." {
+		fmt.Println("Esto no es windows", os)
+	} else {
+		fmt.Println("Esto es windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linux")
+	case "darwin":
+		fmt.Println("Esto es Darwin")
+	default:
+		fmt.Printf("%s \n", os)
+	}
 }
