0e24b6041fa6e38cf48a2619e532947a39ec1eb8
jdacosta92 <juliandaniel.acosta@gmail.com>
2023-08-27 14:43:20 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jdacosta92/godesde0
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/jdacosta92/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
